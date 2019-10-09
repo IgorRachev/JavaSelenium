2d4808dc9d868f0b6e3a329e843af2ca1d77a60a
IgorRachev <53417693+IgorRachev@users.noreply.github.com>
2019-10-09 20:10:30 +0600

    Update ATM.java

--- hw3/ATM.java
+++ hw3/ATM.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class ATM {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         Account account = new Account();
         System.out.println("Текущий баланс " + account.getBalance());
 
         System.out.println("Введите сумму пополнения");
-        double replAmount = scanner.nextDouble();
-        account.putMoney(replAmount);
+        double cashAmount = sc.nextDouble();
+        account.putCash(cashAmount);
 
         System.out.println("Текущий баланс " + account.getBalance());
 
         System.out.println("Введите сумму снятия");
-        double withdrAmount = scanner.nextDouble();
-        account.withdrawMoney(withdrAmount);
+        double cashAmount = sc.nextDouble();
+        account.withdrawCash(cashAmount);
 
         System.out.println("Текущий баланс " + account.getBalance());
 
